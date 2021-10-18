fe7b9c97e082c7541f2738068821278bdc864692
Nedim Akar <nedim.akar53411@gmail.com>
2021-10-18 23:34:18 +0200

    feat: added fingerprint before qr code

--- app/src/main/java/com/passwordless/MainActivity.kt
+++ app/src/main/java/com/passwordless/MainActivity.kt
@@ -6,12 +6,19 @@ import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
 import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.google.zxing.integration.android.IntentIntegrator
 import org.json.JSONObject
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private  var action = 1;
     internal var qrScanIntegrator: IntentIntegrator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +26,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         qrScanIntegrator = IntentIntegrator(this)
         qrScanIntegrator?.setOrientationLocked(true)
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, object: BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(applicationContext, "Authentication error: $errString", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                qrScanIntegrator?.initiateScan()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(applicationContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
         val scanBtn = findViewById(R.id.scanbtn) as Button
         // set on-click listener
-        scanBtn.setOnClickListener { qrScanIntegrator?.initiateScan() }
+        scanBtn.setOnClickListener {
+            authFingerPrint()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -31,6 +66,10 @@ class MainActivity : AppCompatActivity() {
             if (result.contents == null) {
                 Toast.makeText(this, "result null", Toast.LENGTH_LONG).show()
             } else {
+                //if (!result.contents.startsWith("passwordless://")) {
+                //    Toast.makeText(this@MainActivity, "No valid content detected!", Toast.LENGTH_SHORT).show()
+                //    return
+                //}
                 val deviceId: String = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
                 val json = JSONObject()
                 json.put("code", result.contents)
@@ -44,6 +83,10 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    fun authFingerPrint() {
+        biometricPrompt.authenticate(promptInfo)
     }
 
 }
